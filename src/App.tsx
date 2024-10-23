cf403a5f8e4eceadfdc2a8af4862d73f88713485
VasiG <vasi.geri@gmail.com>
2024-10-23 22:25:13 +0200

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,73 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
 
 function App() {
+  const [photo, setPhoto] = useState();
+
+  function fetchPembroke() {
+    const doFetchPembroke = async () => {
+      await fetch("https://dog.ceo/api/breed/pembroke/images/random")
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          setPhoto(data.message);
+        });
+    };
+
+    doFetchPembroke();
+  }
+
+  function fetchCardigan() {
+    const doFetchCardigan = async () => {
+      await fetch("https://dog.ceo/api/breed/corgi/cardigan/images/random")
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          setPhoto(data.message);
+        });
+    };
+
+    doFetchCardigan();
+  }
+
+  function fetchDog() {
+    const doFetchDog = async () => {
+      await fetch("https://dog.ceo/api/breeds/image/random")
+        .then((result) => {
+          return result.json();
+        })
+        .then((data) => {
+          setPhoto(data.message);
+        });
+    };
+
+    doFetchDog();
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="top-container">
+      <div>
+        <img className="corgiImg" src={photo} />
+      </div>
+      <div className="btn-container">
+        <div>
+          <button className="corgiBtn" onClick={fetchPembroke}>
+            I want a new PEMBROKE WELSH CORGI!!!
+          </button>
+        </div>
+        <div>
+          <button className="corgiBtn" onClick={fetchCardigan}>
+            I want a new CARDIGAN CORGI!!!
+          </button>
+        </div>
+        <div>
+          <button className="corgiBtn" onClick={fetchDog}>
+            Surprise dog
+          </button>
+        </div>
+      </div>
     </div>
   );
 }
